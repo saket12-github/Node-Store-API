b0af45d7e5abcfaa07b7821cff5bba809a7a95b5
Saket Chaudhari <saket@imageonline.co.in>
2026-05-21 11:50:40 +0530

    Add create, single and all products API

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,15 +1,47 @@
 const productModel = require('../models/product.model.js')
 
-const getAllProductsStatic = async (req, res) => {
-    res.status(200)
-    res.send('Hello this is the Products static page')
+const getAllProducts = async (req, res) => {
+    try {
+        const allProducts = await productModel.find({});
+        res.status(200).json({
+            mesage: "success",
+            data: allProducts
+        });
+    } catch (error) {
+        res.status(500).json({ Message: error.message })
+    }
 }
 
-const getAllProducts = async (req, res) => {
-    res.status(200)
-    res.send('This is the get all products screen')
+const createProduct = async (req, res) => {
+    try {
+        const newProduct = await productModel.create(req.body);
+        res.status(201).json({ message: 'Product created successfully', data: newProduct });
+    } catch (error) {
+        res.status(500).json({ Message: error.message })
+    }
+}
+
+const getSingleProduct = async (req, res) => {
+    try {
+        const { id: product_id } = req.params;
+        const singleProduct = await productModel.find({ _id: product_id });
+        if (!singleProduct) {
+            return res.status(404).json({ Message: "Product not found" });
+        }
+
+        res.status(200).json({
+            message: "success",
+            data: singleProduct
+        });
+    } catch (error) {
+        res.status(500).json({ Message: error.message });
+    }
 }
 
 module.exports = {
-    getAllProducts, getAllProductsStatic
+    getAllProducts,
+    createProduct,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const Router = express.Router()
-const { getAllProducts, getAllProductsStatic } = require('../controllers/product.controller')
+const { getAllProducts, createProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/product.controller')
 
-Router.get('/', getAllProducts)
-Router.get('/static', getAllProductsStatic)
+Router.get('/products', getAllProducts)
+Router.post('/products', createProduct)
+Router.get('/products/:id', getSingleProduct)
+Router.put('/products/:id', updateProduct)
+Router.delete('/products/:id', deleteProduct)
 
 module.exports = Router
