ecd63a8b0bb0e18e4d8f537b8291ddfd96734207
Saket Chaudhari <saket@imageonline.co.in>
2026-05-22 17:02:12 +0530

    Implement pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -22,6 +22,12 @@ const getAllProducts = async (req, res) => {
         products = products.sort('createdAt')
     }
 
+    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+
+    products = products.skip(skip).limit(limit);
+
     if (fields) {
         const fieldsList = fields.split(',').join(' ');
         products = products.select(fieldsList);
